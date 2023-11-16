79c1e2bb60200ae4bf0624d285d4dacce7adf174
aaronbenham <aaron.benham123@gmail.com>
2023-11-16 12:09:13 +0000

    Add combined form elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,15 +1,45 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import PhoneInput from "react-phone-number-input/input"
+
+import logo from "../images/dog-icon.png"
 
 function CombinedIntputs(subject) {
-    const [name, setName] = useState("");
+    const [name, setName] = useState(""); // dog or walker name
+    const [phoneNum, setPhoneNum] = useState(); // owner or walker phone number
+    const [birthDate, setBirthDate] = useState(); // dog or walker birthday
+
 
     return (
-        <div>
-            <label>Name:</label>
-            <input onChange={(e) => setName(e.target.value)}/>
+        <>
+            <div className="combined-input-form">
+                <label>Name:</label>
+                <input onChange={(e) => setName(e.target.value)}/>
+                
+                <br/>
 
-            <button onClick={() => console.log(name)}>Submit</button>
-        </div>
+                {subject === "Dog" ? <label>Owner Number:</label>:<label>Number:</label>}
+                <PhoneInput country="UK" value={phoneNum} onChange={setPhoneNum}/>
+                
+                <br/>
+
+                <label>Date of Birth:</label>
+                <input type="date" value={birthDate} onChange={setBirthDate}/>
+
+                <br/>
+
+                <label>Address:</label>
+                <input onChange={(e) => setName(e.target.value)}/>
+            </div>
+
+            <div className="button-container">
+                <Link to="/">
+                        <button className="form-button">Cancel</button>
+                </Link>
+
+                <button className="form-button" onClick={() => console.log(name)}>Submit</button>
+            </div>
+        </>
     )
 }
 
@@ -26,6 +56,10 @@ function IndividualInputs(subject) {
 function RegisterPage(subject) {
     return (
         <div className="page">
+            <div id="logo-container">
+                <img id="form-dog-logo" src={logo} alt="dog icon logo"/>
+            </div>
+
             <h1 className="page-title">Register {subject}</h1>
 
             {CombinedIntputs(subject)}
