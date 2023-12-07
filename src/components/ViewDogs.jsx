dbe123f4c9c5027359bddf8086e5b3128f8d9ccf
aaronbenham <aaron.benham123@gmail.com>
2023-12-07 18:05:29 +0000

    GET API calls implemented

--- src/components/ViewDogs.jsx
+++ src/components/ViewDogs.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../images/dog-icon.png"
 
 function ViewDogs() {
+    const [data, setData] = useState(null)
 
-    async function GET_API() {
-        const [allData, setAllData] = useState()
-    
-        await fetch("https://pxuvfvxvph.execute-api.eu-west-2.amazonaws.com/beta/dogs", {method: "GET"})
+    useEffect(() => {
+        fetch("https://pxuvfvxvph.execute-api.eu-west-2.amazonaws.com/beta/dogs", {method: "GET"})
         .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-        })
-    
-    }
-    
+        .then(json => setData(json)) 
+        .catch(error => console.error(error))
+    }, [])
+
+    console.log(data)
+
     return (
         <>
         <div className="page">
@@ -23,6 +23,15 @@ function ViewDogs() {
             </div>
 
             <h1 className="page-title">All Dogs</h1>
+
+            {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <h2>Loading...</h2>}
+
+            <div className="button-container">
+                <Link to="/">
+                        <button className="form-button">Cancel</button>
+                </Link>
+            </div>
+
         </div>
         </>
     )
