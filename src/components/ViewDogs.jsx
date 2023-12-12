2bafd868d6f544f200b588d9c79cf22ded5a340f
aaronbenham <aaron.benham123@gmail.com>
2023-12-12 21:24:43 +0000

    create display tables with css

--- src/components/ViewDogs.jsx
+++ src/components/ViewDogs.jsx
@@ -3,17 +3,44 @@ import { Link } from "react-router-dom";
 
 import logo from "../images/dog-icon.png"
 
+
+
 function ViewDogs() {
     const [data, setData] = useState(null)
 
     useEffect(() => {
         fetch("https://pxuvfvxvph.execute-api.eu-west-2.amazonaws.com/beta/dogs", {method: "GET"})
         .then((response) => response.json())
-        .then(json => setData(json)) 
+        .then(json => setData(json["body"])) 
         .catch(error => console.error(error))
     }, [])
 
-    console.log(data)
+    function DogTable(data) {
+        return (
+            <div className="table-container">
+                <table className="table-display">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Breed</th>
+                            <th>Date of birth</th>
+                        </tr>
+                        {data.map((val, key) => {
+                            return (
+                                <tr id="table-elements" key={key}>
+                                    <td>{val.dog_ID}</td>
+                                    <td>{val.Name}</td>
+                                    <td>{val.Breed}</td>
+                                    <td>{val.DateOfBirth}</td>
+                                </tr>
+                            )
+                        })}
+                    </thead>
+                </table>
+            </div>
+        )
+    }
 
     return (
         <>
@@ -24,8 +51,8 @@ function ViewDogs() {
 
             <h1 className="page-title">All Dogs</h1>
 
-            {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <h2>Loading...</h2>}
-
+            {data ? DogTable(data) : <h2 className="loading-header">Loading...</h2>}
+            
             <div className="button-container">
                 <Link to="/">
                         <button className="form-button">Cancel</button>
--- src/components/ViewWalkers.jsx
+++ src/components/ViewWalkers.jsx
@@ -9,11 +9,58 @@ function ViewWalkers() {
     useEffect(() => {
         fetch("https://pxuvfvxvph.execute-api.eu-west-2.amazonaws.com/beta/walkers", {method: "GET"})
         .then((response) => response.json())
-        .then(json => setData(json)) 
+        .then(json => setData(json["body"])) 
         .catch(error => console.error(error))
     }, [])
 
-    console.log(data)
+    function WalkerTable(data) {
+        return (
+            <div className="table-container">
+                <table className="table-display">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th colSpan={7}>Availability
+                                <th id="mon-head">Mon</th>
+                                <th id="tue-head">Tue</th>
+                                <th id="wed-head">Wed</th>
+                                <th id="thu-head">Thu</th>
+                                <th id="fri-head">Fri</th>
+                                <th id="sat-head">Sat</th>
+                                <th id="sun-head">Sun</th>
+                            </th>
+                            <th>Mobile number</th>
+                            <th>Address</th>
+                            <th>Date of birth</th>
+                        </tr>
+                        {data.map((val, key) => {
+                            const availability = val.Availability.toString().split(",")
+                    
+                            return (
+                                <tr id="table-elements" key={key}>
+                                    <td>{val.walker_ID}</td>
+                                    <td>{val.Name}</td>
+                                    
+                                    <td className="day" id="mon">{availability[0]}</td>
+                                    <td className="day" id="tue">{availability[1]}</td>
+                                    <td className="day" id="wed">{availability[2]}</td>
+                                    <td className="day" id="thu">{availability[3]}</td>
+                                    <td className="day" id="fri">{availability[4]}</td>
+                                    <td className="day" id="sat">{availability[5]}</td>
+                                    <td className="day" id="sun">{availability[6]}</td>
+
+                                    <td>{val.PhoneNumber}</td>
+                                    <td>{val.Address}</td>
+                                    <td>{val.DateOfBirth}</td>
+                                </tr>
+                            )
+                        })}
+                    </thead>
+                </table>
+            </div>
+        )
+    }
 
     return (
         <>
@@ -24,7 +71,7 @@ function ViewWalkers() {
 
             <h1 className="page-title">All Walkers</h1>
 
-            {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <h2>Loading...</h2>}
+            {data ? WalkerTable(data) : <h2 className="loading-header">Loading...</h2>}
 
             <div className="button-container">
                 <Link to="/">
